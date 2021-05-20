83c4e6eaa60972a3724e1d1a92bd7a72f69e279e
Tyree <t9tyree@hotmail.co.uk>
2021-05-20 11:27:28 +0100

    added elif

---
+++ Control_flow.py
@@ -0,0 +1,14 @@
+#control flow with if, elif and else
+#loops: while, for
+
+# if statment
+weather = "rain"
+
+if weather == "sunny": #if the boolean value = true then run next line of code
+    print("Enjoy the weather")
+elif weather == "rain":
+    print("Bring an umbrella, its raining")
+elif weather == "cloudy":
+    print("No sun, but lucky no rain yet")
+else:
+    print("Something went wrong")
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Control Flow
-
+### Very important in any programming language, used to control the flow of your conditions and decisions
 - if, elif and else conditions
 
